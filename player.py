f2da2aaa69c675e899874184396c9a122fdcc0fa
turbek <turbek93@gmail.com>
2016-09-24 14:08:23 +0200

    baszki

--- player.py
+++ player.py
@@ -70,35 +70,36 @@ class Player:
         pass
 
     def think(self, game_state):
-        # if len(game_state["community_cards"]) == 0:
-            # bet = 1000
+        if len(game_state["community_cards"]) == 0:
+            bet = 1000
         for player in game_state["players"]:
             if player["name"] == "piton":
                 for cards in player["hole_cards"]:
                     self.hand.append(cards)
-                    # for card, value in cards.items():
-                    #     if card == "rank":
-                    #         if value in self.low_cards:
-                    #             bet = 0
-        # if self.hand[0]["rank"] == self.hand[1]["rank"]:
-        #     bet = 1000
+                    for card, value in cards.items():
+                        if card == "rank":
+                            if value in self.low_cards:
+                                bet = 0
+        if self.hand[0]["rank"] == self.hand[1]["rank"]:
+            bet = 1000
+        return bet
 
-        for odd in self.odds:
-            possible_hand = []
-            for card in odd[:2]:
-                possible_hand.append(card)
-            if (self.hand[0]["rank"] == possible_hand[0]["rank"] and self.hand[1]["rank"] == possible_hand[1]["rank"] or self.hand[1]["rank"] == possible_hand[0]["rank"] and self.hand[0]["rank"] == possible_hand[1]["rank"]) and ((possible_hand[0]["suit"] == possible_hand[1]["suit"]) == suit_check()):
-                    if int(possible_hand[0]["odds"]) > 2:
-                        bet = 6000
-                        break
-                    else:
-                        bet = 50
-                        break
-            return bet
+        # for odd in self.odds:
+        #     possible_hand = []
+        #     for card in odd[:2]:
+        #         possible_hand.append(card)
+        #     if (self.hand[0]["rank"] == possible_hand[0]["rank"] and self.hand[1]["rank"] == possible_hand[1]["rank"] or self.hand[1]["rank"] == possible_hand[0]["rank"] and self.hand[0]["rank"] == possible_hand[1]["rank"]) and ((possible_hand[0]["suit"] == possible_hand[1]["suit"]) == suit_check()):
+        #             if int(possible_hand[0]["odds"]) > 2:
+        #                 bet = 6000
+        #                 break
+        #             else:
+        #                 bet = 50
+        #                 break
+        #     return bet
         # if len(game_state["community_cards"]) == 0:
-
-    def suit_check(self):
-        return self.hand[0]["suit"] == self.hand[1]["suit"]
+    #
+    # def suit_check(self):
+    #     return self.hand[0]["suit"] == self.hand[1]["suit"]
     #
     # def handreturn(self, game_state):
     #     self.hand = []
